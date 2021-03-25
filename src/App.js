8af3ad959afd4d5d8a96d517cadfeba1bac3967b
Nirmeet <nirmeet.baweja@gmail.com>
2021-03-25 19:43:25 +0000

    Level 3 Completed
    Sort the individual country's scores in descending order.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import AllCountryScores from "./data/AllCountryScores";
 import ScoreBoard from "./ScoreBoard";
 import sortByCountry from "./sortByCountry";
+import sortByScore from "./sortByScore";
 
 import Heading from "./Heading";
 
@@ -9,6 +10,11 @@ import "./App.css";
 
 function App() {
   AllCountryScores.sort(sortByCountry);
+  AllCountryScores.forEach((oneCountryScores) => {
+    console.log(oneCountryScores.scores);
+    oneCountryScores.scores.sort(sortByScore);
+  });
+
   return (
     <div className="App">
       <Heading />
---
+++ src/sortByScore.js
@@ -0,0 +1,3 @@
+const sortByScore = (a, b) => -1 * (a.s - b.s);
+
+export default sortByScore;
